f66a78d68a565a271d8f2723a32c2f707be4e430
mingman <mingyuchoi514@gmail.com>
2020-11-23 00:01:04 +0900

    ec2 nodemailer set

--- routes/register.js
+++ routes/register.js
@@ -8,12 +8,13 @@ const nodemailer = require('nodemailer')
 
 const smtpTransport = nodemailer.createTransport({
     service: "Gmail",
+    pool: true,
+    host: "admin.skklub.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.MAIL_ADDRESS,
         pass: process.env.MAIL_PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: false
     }
   });
 
