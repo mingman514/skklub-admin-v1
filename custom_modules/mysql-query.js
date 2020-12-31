c3bc21bd08bbfd8de6e244f27c292f9d3ed68969
mingman <mingyuchoi514@gmail.com>
2020-12-31 12:16:13 +0900

    set sql character-set-utf8mb4

--- custom_modules/mysql-query.js
+++ custom_modules/mysql-query.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    multipleStatements: true
+    multipleStatements: true,
+    charset : 'utf8mb4'
 });
 connection.connect();
 
